7553b2da45b9a151e826612dfd4990ae0e767a2a
Viktor von Westarp <94043096+MrMetube@users.noreply.github.com>
2022-07-22 19:41:27 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,20 +1,15 @@
---[[ Lua code. See documentation: https://api.tabletopsimulator.com/ --]]
-
---[[ The onLoad event is called after the game save finishes loading. --]]
 function onLoad()
-	print("UPDATED")
+    print("Updatet!")
     checkForUpdate()
 end
 
-ScriptVersion = 3
+ScriptVersion = 4
 ScriptClass = 'MyUtility'
 
 function checkForUpdate()
-    -- WebRequest.get("https://api.github.com/repos/MrMetube/TTS/contents/test.json", function(res)
     WebRequest.get("https://raw.githubusercontent.com/MrMetube/TTS/main/test.json", function(res)
         if (not(res.is_error)) then
             local response = JSON.decode(res.text)
-            table.print(response)
             if (response[ScriptClass] > ScriptVersion) then
                 print('New Version ('..response[ScriptClass]..') of '..ScriptClass..' is available!')
                 -- install update?
@@ -24,36 +19,15 @@ function checkForUpdate()
             error(res)
         end
     end)
-
-
-    -- WebRequest.get("https://api.github.com/repos/MrMetube/TTS/contents/Test.json", function(request)
-    --     if request.is_error then
-    --         log(request.error)
-    --     else
-    --         print(request.text)
-    --     end
-    -- end)
 end
 
 function installUpdate(newVersion)
-	print('[33ff33]Installing Upgrade to '..ScriptClass..'['..tostring(newVersion)..']')
-	WebRequest.get('https://github.com/MrMetube/TTS/blob/main/test.lua', function(res)
-        if (not(res.is_error)) then
-		self.setLuaScript(res.text)
-		self.reload()
-		print('[33ff33]Installation Successful[-]')
-        else
-            error(res)
-        end
+	print('[33ff33]Installing Upgrade to '..ScriptClass..'('..tostring(newVersion)..')[-]')
+	WebRequest.get('https://raw.githubusercontent.com/MrMetube/TTS/main/test.lua', function(res)
+        if res.is_error then return error(res) end
+        print(res.text)
+        self.setLuaScript(res.text)
+        -- self.reload()
+        print('[33ff33]Installation Successful[-]')
     end)
-end
-
-function table.print(input,surkey)
-if surkey == nil then surkey = "" end 
-    for k,v in pairs(input) do
-        k=surkey..""..k
-        if type(v)=="table" then table.print(v,k)
-        elseif type(v)=="boolean" then print(k..": "..tostring(v))
-        else print(k..": "..v) end
-    end
 end
