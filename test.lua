f3c558268deb939314ddf3eaf8affb653166b483
Viktor von Westarp <94043096+MrMetube@users.noreply.github.com>
2022-07-22 17:58:29 +0200

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,37 @@
+--[[ Lua code. See documentation: https://api.tabletopsimulator.com/ --]]
+
+--[[ The onLoad event is called after the game save finishes loading. --]]
+function onLoad()
+    print('github!')
+end
+
+ScriptVersion = 4
+ScriptClass = 'MyUtility'
+
+function checkForUpdate()
+    WebRequest.get('yourgithubfile.json', function(res)
+        if (not(res.is_error)) then
+            local response = JSON.decode(res.text)
+		if (response[ScriptClass] > ScriptVersion) then
+			print('New Version ('..response[ScriptClass]..') of '..ScriptClass..' is available!')
+			--install update?
+			installUpdate(response[ScriptClass])
+		end
+        else
+            error(res)
+        end
+    end)
+end
+
+function installUpdate(newVersion)
+	print('[33ff33]Installing Upgrade to '..ScriptClass..'['..tostring(newVersion)..']')
+	WebRequest.get('yourgithubfile.lua', function(res)
+        if (not(res.is_error)) then
+		self.setLuaScript(res.text)
+		self.reload()
+		print('[33ff33]Installation Successful[-]')
+        else
+            error(res)
+        end
+    end)
+end
