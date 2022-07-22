8ad563a07360fbc5bd3a831f30d188c3df5110f2
Viktor von Westarp <94043096+MrMetube@users.noreply.github.com>
2022-07-22 19:30:14 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,30 +2,42 @@
 
 --[[ The onLoad event is called after the game save finishes loading. --]]
 function onLoad()
-    print('github!')
+	print("UPDATED")
+    checkForUpdate()
 end
 
-ScriptVersion = 4
+ScriptVersion = 3
 ScriptClass = 'MyUtility'
 
 function checkForUpdate()
-    WebRequest.get('yourgithubfile.json', function(res)
+    -- WebRequest.get("https://api.github.com/repos/MrMetube/TTS/contents/test.json", function(res)
+    WebRequest.get("https://raw.githubusercontent.com/MrMetube/TTS/main/test.json", function(res)
         if (not(res.is_error)) then
             local response = JSON.decode(res.text)
-		if (response[ScriptClass] > ScriptVersion) then
-			print('New Version ('..response[ScriptClass]..') of '..ScriptClass..' is available!')
-			--install update?
-			installUpdate(response[ScriptClass])
-		end
+            table.print(response)
+            if (response[ScriptClass] > ScriptVersion) then
+                print('New Version ('..response[ScriptClass]..') of '..ScriptClass..' is available!')
+                -- install update?
+                installUpdate(response[ScriptClass])
+            end
         else
             error(res)
         end
     end)
+
+
+    -- WebRequest.get("https://api.github.com/repos/MrMetube/TTS/contents/Test.json", function(request)
+    --     if request.is_error then
+    --         log(request.error)
+    --     else
+    --         print(request.text)
+    --     end
+    -- end)
 end
 
 function installUpdate(newVersion)
 	print('[33ff33]Installing Upgrade to '..ScriptClass..'['..tostring(newVersion)..']')
-	WebRequest.get('yourgithubfile.lua', function(res)
+	WebRequest.get('https://github.com/MrMetube/TTS/blob/main/test.lua', function(res)
         if (not(res.is_error)) then
 		self.setLuaScript(res.text)
 		self.reload()
@@ -34,4 +46,14 @@ function installUpdate(newVersion)
             error(res)
         end
     end)
+end
+
+function table.print(input,surkey)
+if surkey == nil then surkey = "" end 
+    for k,v in pairs(input) do
+        k=surkey..""..k
+        if type(v)=="table" then table.print(v,k)
+        elseif type(v)=="boolean" then print(k..": "..tostring(v))
+        else print(k..": "..v) end
+    end
 end
